6640e9a44b957576e714d02b3440a6e76b95ab66
suryatejaballa <suryatejaballa5@gmail.com>
2025-08-09 10:28:53 +0530

    Trigger redeploy with homepage fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+    Hello from my React CI/CD GitHub Pages deployment!
+      
         </p>
         <a
           className="App-link"
